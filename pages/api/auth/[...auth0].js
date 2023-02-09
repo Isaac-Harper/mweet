cf7105bc6e7d3089c8d0dac90253b10333685000
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2023-02-09 11:33:24 -0700

    login

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -16,9 +16,15 @@ const afterCallback = async (req, res, session) => {
 export default handleAuth({
 	async callback(req, res) {
 		try {
-			await handleCallback(req, res, { afterCallback });
+			await handleCallback	(req, res, { afterCallback });
 		} catch (error) {
 			res.status(error.status || 500).end(error.message);
 		}
 	},
+	
+	async login(req, res) {
+	    await handleLogin(req, res, {
+	      		returnTo: "/home",
+	    });
+  	},
 });
